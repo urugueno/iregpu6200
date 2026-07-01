72c7275a02ed2b59a7283db0199ea46834bd521b
urugueno <ueno12221222@gmail.com>
2026-07-01 18:29:24 +0900

    2test

--- device0.sh
+++ device0.sh
@@ -7,5 +7,5 @@ CUDA_VISIBLE_DEVICES=0 python -u Illuminance_bbox/main.py \
     --window_size 41 --sub_window_size 37 --stride 1 --scale full --model_mode time_sensor \
     --num_queries 10 --enc_layers 3 --dec_layers 3 --num_classes 4 --environment none \
     --num_sensors 36 --k_neighbors 36 --exp_name tmp --sensor_dropout_rate 0.1 --random_sensors;
-test
+
 # nohup ./device0.sh > ./device0.txt &
